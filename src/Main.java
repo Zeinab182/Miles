6e459b081339b5cbe81131fb9d244743e8f0a91e
Zeinab <Zeinab182@mail.ru>
2020-07-24 00:22:33 +0300

    added src

---
+++ src/Main.java
@@ -0,0 +1,8 @@
+public class Main {
+    public static void main(String[] args) {
+        int ticketprice = 16587;
+        int rubles = 20;
+        int bonus = ticketprice / rubles;
+        System.out.println(bonus);
+    }
+}
